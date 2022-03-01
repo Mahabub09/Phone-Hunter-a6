6268d4fb8cb481ea768fad4322eafb9e26d99c11
mahbub09 <mahabub895alam@gmail.com>
2022-03-02 05:02:54 +0600

    error handle done

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@ const searchYourPhone = () => {
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
     searchField.value = '';
-    const errorNull = document.getElementById("null-message")
+    const errorMessage = document.getElementById("error-message")
+    //------ error handle null message ------//
     if (searchText == '') {
 
-        errorNull.style.display = 'block'
+        errorMessage.innerText = "PLease Write Something To Search"
     }
     else {
-        errorNull.style.display = 'none'
+
         const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
             .then(res => res.json())
@@ -18,27 +19,39 @@ const searchYourPhone = () => {
 }
 
 const displaySearchResult = data => {
-    const searchResult = document.getElementById('search-result')
-    searchResult.textContent = '';
-    data.forEach(data => {
-        console.log(data)
-        const div = document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML = `<div class="col">
+    const phoneDetails = document.getElementById('phone-details')
+    phoneDetails.innerHTML = '';
+    const errorMessage = document.getElementById('error-message')
+    if (data.length === 0) {
+        errorMessage.innerText = " Sorry!! No Phone Found"
+
+    }
+    else {
+
+        const searchResult = document.getElementById('search-result')
+        searchResult.textContent = '';
+        data.forEach(info => {
+            if (data.indexOf(info) < 20) {
+                errorMessage.innerText = ""
+                const div = document.createElement('div')
+                div.classList.add('col')
+                div.innerHTML = `<div class="col">
         <div class="card">
         <div class="card-body">
-                <h5 class="card-title text-center">${data.phone_name}</h5>
-                <p class="card-text text-center">${data.brand}</p>
+                <h5 class="card-title text-center">${info.phone_name}</h5>
+                <p class="card-text text-center">${info.brand}</p>
             </div>
-            <img src="${data.image}" class="card-img-top w-75 mx-auto" alt="...">
-            <a onclick="loadDetail('${data.slug}')" href="#" class="btn btn-outline-success w-50 mx-auto my-3">Specs</a>
+            <img src="${info.image}" class="card-img-top w-75 mx-auto" alt="...">
+            <a onclick="loadDetail('${info.slug}')" href="#" class="btn btn-outline-success w-50 mx-auto my-3">Specs</a>
         </div>
         
         </div>
         `
-        searchResult.appendChild(div)
+                searchResult.appendChild(div)
+            }
 
-    })
+        })
+    }
 
 }
 const loadDetail = id => {
@@ -50,11 +63,19 @@ const loadDetail = id => {
 
 }
 const displayPhoneDetail = data => {
-    console.log(data)
+
     const phoneDetails = document.getElementById('phone-details')
     phoneDetails.textContent = '';
     const div = document.createElement('div')
     div.classList.add('card')
+    const releaseDate = (date) => {
+        if (date === '') {
+            return 'Relase Date Not Found';
+        }
+        else {
+            return date;
+        }
+    }
     div.innerHTML = `
     <img src="${data.data.image}" class="card-img-top" alt="...">
                 <div class="card-body mb-3">
@@ -65,7 +86,7 @@ const displayPhoneDetail = data => {
                   <li><span class= "fw-bold">Memory</span>: ${data.data.mainFeatures.memory}</li>
                   <li><span class= "fw-bold">Storage</span>: ${data.data.mainFeatures.storage}</li>
                   </ul>
-                  <p class="card-text"><small class="text-muted">${data.data.releaseDate}</small></p>
+                  <p class="card-text"><small class="text-muted">${releaseDate(data.data.releaseDate)}</small></p>
                   <div class="accordion" id="accordionExample">
                     <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
@@ -77,12 +98,12 @@ const displayPhoneDetail = data => {
                     <div class="accordion-body">
                     <ul>
                   <li><span class= "fw-bold">Sensors</span>: ${data.data.mainFeatures.sensors}</li>
-                  <li><span class= "fw-bold">Bluetooth</span>: ${data.data.others.Bluetooth}</li>
-                  <li><span class= "fw-bold">GPS</span>: ${data.data.others.GPS}</li>
-                  <li><span class= "fw-bold">NFC</span>: ${data.data.others.NFC}</li>
-                  <li><span class= "fw-bold">Radio</span>: ${data.data.others.Radio}</li>
-                  <li><span class= "fw-bold">USB</span>: ${data.data.others.USB}</li>
-                  <li><span class= "fw-bold">WLAN</span>: ${data.data.others.WLAN}</li>
+                  <li><span class= "fw-bold">Bluetooth</span>: ${data.data.others ? data.data.others.Bluetooth : "Bluetooth is not Available"}</li>
+                  <li><span class= "fw-bold">GPS</span>: ${data.data.others ? data.data.others.GPS : "GPS is not Available"}</li>
+                  <li><span class= "fw-bold">NFC</span>: ${data.data.others ? data.data.others.NFC : "NFC is not Available"}</li>
+                  <li><span class= "fw-bold">Radio</span>: ${data.data.others ? data.data.others.Radio : "Radio is not Available"}</li>
+                  <li><span class= "fw-bold">USB</span>: ${data.data.others ? data.data.others.USB : "USB is not Available"}</li>
+                  <li><span class= "fw-bold">WLAN</span>: ${data.data.others ? data.data.others.WLAN : "WLAN is not Availble"}</li>
                   
                 </ul>
                     </div>
